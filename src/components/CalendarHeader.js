ac2d13e484613a672528c75ef6ed38a61244343d
Thanzeel Hassan <thanzeelhassan@gmail.com>
2026-07-13 16:20:47 +0530

    Fix the scroll issue

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -17,7 +17,7 @@ export default function CalendarHeader(props) {
     setMonthIndex(
       monthIndex === dayjs().month()
         ? monthIndex + Math.random()
-        : dayjs().month()
+        : dayjs().month(),
     );
   }
   // const options = [
@@ -30,37 +30,31 @@ export default function CalendarHeader(props) {
   // ];
 
   return (
-    <header className="px-4 py-2 flex items-center">
-      <img src={logo} alt="calender" className="mr-2 w-12 h-12" />
-      <h1 className="mr-10 text-xl text-gray-500 fond-bold">Calendar</h1>
-      <button onClick={handleReset} className="border rounded py-2 px-4 mr-5">
-        Today
-      </button>
-      <button onClick={handlePrevMonth}>
-        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
-          chevron_left
-        </span>
-      </button>
-      <button onClick={handleNextMonth}>
-        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
-          chevron_right
-        </span>
-      </button>
-      <h2 className="ml-4 text-xl text-black-500 font-bold">
-        {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
-      </h2>
-
-      <div
-        className="container-right float-right right-0 ml-80"
-        style={{ backgroundColor: "lightblue", marginLeft: "75%" }}
-      >
-        <button
-          className="float-right border rounded py-2 px-4 right-0"
-          onClick={toggleWeekOrMonth}
-        >
-          {weekOrMonth}
+    <header className="px-4 py-2 flex items-center justify-between">
+      <div className="flex items-center gap-4">
+        <img src={logo} alt="calender" className="w-12 h-12" />
+        <h1 className="text-xl text-gray-500 font-bold">Calendar</h1>
+        <button onClick={handleReset} className="border rounded py-2 px-4">
+          Today
         </button>
+        <button onClick={handlePrevMonth}>
+          <span className="material-icons-outlined cursor-pointer text-gray-600">
+            chevron_left
+          </span>
+        </button>
+        <button onClick={handleNextMonth}>
+          <span className="material-icons-outlined cursor-pointer text-gray-600">
+            chevron_right
+          </span>
+        </button>
+        <h2 className="text-xl text-black-500 font-bold">
+          {dayjs(new Date(dayjs().year(), monthIndex)).format("MMMM YYYY")}
+        </h2>
       </div>
+
+      <button className="border rounded py-2 px-4" onClick={toggleWeekOrMonth}>
+        {weekOrMonth}
+      </button>
     </header>
   );
 }
