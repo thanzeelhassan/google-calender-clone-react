31da70ed0a451b5d4c90b8f5dbbc7fe107b78b51
Thanzeel Hassan <thanzeelhassan@gmail.com>
2026-07-14 11:18:08 +0530

    Add google auth

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import ContextWrapper from "./context/ContextWrapper";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID || "1234567890-placeholder.apps.googleusercontent.com";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ContextWrapper>
-      <App />
-    </ContextWrapper>
+    <GoogleOAuthProvider clientId={clientId}>
+      <ContextWrapper>
+        <App />
+      </ContextWrapper>
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
 
